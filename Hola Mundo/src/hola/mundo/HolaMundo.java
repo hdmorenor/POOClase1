88156466787c211e1b59cd1659d37329ad4e5139
Estudiante <Estudiante@10.203.1.193>
2017-08-01 15:32:07 -0500

    HOLA MUNDO

---
+++ Hola Mundo/src/hola/mundo/HolaMundo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class HolaMundo {
+
+    
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int entero= 5;
+        
+        
+        for (int i = 0; i < 2; i++) {
+             System.out.println("Hola mundo");
+             System.out.println(entero);
+        }
+        
+        // TODO code application logic here
+    }
+    
+}
